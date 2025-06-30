f564fa257189fc23c324153ddd965cfc4c69077a
ToadLobe <pablisgalbraith@gmail.com>
2025-06-30 23:50:17 +0100

    Add testing project

---
+++ tempoary/scripts/c3main.js
@@ -0,0 +1,8 @@
+import "./../gl-matrix.js";
+import "./../gltfData.js";
+import "./../boneBuffer.js";
+import "./../objectBuffer.js";
+import "./../gltfModel.js";
+import "./../gltfModelW.js";
+import "./c3runtime.js";
+import "./objRefTable.js";

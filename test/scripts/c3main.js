fb689a7edbb81925c08728db2274729e5d096eff
ToadLobe <pablisgalbraith@gmail.com>
2025-07-07 17:05:12 +0100

    Change privacy policy

--- test/scripts/c3main.js
+++
@@ -1,8 +0,0 @@
-import "./../gl-matrix.js";
-import "./../gltfData.js";
-import "./../boneBuffer.js";
-import "./../objectBuffer.js";
-import "./../gltfModel.js";
-import "./../gltfModelW.js";
-import "./c3runtime.js";
-import "./objRefTable.js";
